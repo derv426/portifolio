d970d92c0d2d4e69759d9623b147397cf9fc03cc
derv426 <joaopedrodev23@outlook.com>
2025-03-03 12:57:37 -0300

    add

---
+++ src/javascript/animation.js
@@ -0,0 +1,27 @@
+
+
+ScrollReveal().reveal('.container-content-sobre', {
+  delay: 500,        // Atraso antes da animação iniciar (200ms)
+  distance: '50px',  // Distância do movimento
+  duration: 950,     // Duração da animação (800ms)
+  easing: 'ease-in-out', // Suavização da animação
+  origin: 'rigth',     // Animação vem de cima
+  opacity: 0,        // Começa invisível
+  scale: 0.9,        // Começa levemente menor
+  reset: true,       // Reanima ao rolar para cima novamente
+  mobile: true
+});
+
+
+ScrollReveal().reveal('.container-skills', {
+  delay: 500,        // Atraso antes da animação iniciar (200ms)
+  distance: '50px',  // Distância do movimento
+  duration: 950,     // Duração da animação (800ms)
+  easing: 'ease-in-out', // Suavização da animação
+  origin: 'left',     // Animação vem de cima
+  opacity: 0,        // Começa invisível
+  scale: 0.9,        // Começa levemente menor
+  reset: true,       // Reanima ao rolar para cima novamente
+  mobile: true
+});
+
